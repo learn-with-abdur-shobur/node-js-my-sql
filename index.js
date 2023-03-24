c43472773ce1ae25e5ef01ac2db37e0bfc030e27
abdur-shobur <abdurshobur.developer@gmail.com>
2023-03-24 06:34:19 +0600

    add

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+const express = require('express');
 const mysql = require('mysql');
-
-const config = {
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.listen(3000, () => {
+	console.log('connect');
+});
+var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: '',
-	database: 'school',
-};
+	database: 'sos',
+});
+connection.connect();
+connection.query('SELECT * FROM `sizes`', function (error, results) {
+	if (error) throw error;
+	console.log('The solution is: ', results);
+});
+connection.end();
 
-const con = mysql.createConnection(config);
-con.connect(function (er) {
-	if (er) {
-		console.log(er);
-	} else {
-		console.log('succs');
-	}
+app.get('/', (req, res) => {
+	connection.connect();
+	connection.query('SELECT * FROM `sizes`', function (error, results) {
+		// if (error) throw error;
+		// console.log(error);s
+		// console.log('The solution is: ', results);
+		res.json(results);
+	});
+	connection.end();
 });
